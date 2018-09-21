8810e0ea98d7cb2bc57521cf08e9b87ae6b59e15
petkoxray <petkoxray@gmail.com>
2018-09-21 15:43:58 +0300

    updated config

--- config.py
+++ config.py
@@ -6,14 +6,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     # App configurations
     SECRET_KEY = os.environ.get('SECRET_KEY') or "very-secret-key"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+                              or 'sqlite:///' + os.path.join(basedir, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    ELASTICSEARCH_URL = 'http://localhost:9200'
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 
     # App Constants
     POSTS_PER_PAGE = 15
